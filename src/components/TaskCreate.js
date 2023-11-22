200ed172b2bcf2585cd8972925353ab8c102842d
Burak <burakturk@gmail.com>
2023-11-22 17:23:53 +0300

    Task

---
+++ src/components/TaskCreate.js
@@ -0,0 +1,13 @@
+function TaskCreate() {
+  return (
+    <div>
+      <h3>Lütfen Task Ekleyiniz!</h3>
+
+      <form>
+        <label>Başlık</label>
+      </form>
+    </div>
+  );
+}
+
+export default TaskCreate;
