bbabe47755dc7701d9dc14f7eefdf62ba376829b
Burak <burakturk@gmail.com>
2023-11-23 11:44:32 +0300

    TaskList

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,11 +1,14 @@
 function TaskShow({ task }) {
   console.log(task);
-  return <div>
+  return <div className="task-show">
     <h3>Göreviniz</h3>
     <p>{task.title}</p>
     <h3>Yapılacaklar</h3>
     <p>{task.taskDec}</p>
+    <button>Sil</button>
+  <button>Güncelle</button>
   </div>;
+  
 }
 
 export default TaskShow;
