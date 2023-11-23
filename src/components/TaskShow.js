52ba3c28e799418bc492e32d4d86a64c863250b6
Burak <burakturk@gmail.com>
2023-11-23 11:59:10 +0300

    TakShow2

--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -1,14 +1,18 @@
 function TaskShow({ task }) {
   console.log(task);
-  return <div className="task-show">
-    <h3>Göreviniz</h3>
-    <p>{task.title}</p>
-    <h3>Yapılacaklar</h3>
-    <p>{task.taskDec}</p>
-    <button>Sil</button>
-  <button>Güncelle</button>
-  </div>;
-  
+  return (
+    <div className="task-show">
+      <h3 className="task-title">Göreviniz</h3>
+      <p>{task.title}</p>
+      <h3 className="task-title">Yapılacaklar</h3>
+      <p>{task.taskDec}</p>
+      
+      <div>
+      <button className="task-delete">Sil</button>
+      <button className="task-edit">Güncelle</button>
+    </div>
+    </div>
+  );
 }
 
 export default TaskShow;
